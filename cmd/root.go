c248ce60b17a14d2c82e16d383d630079bccdb19
Tim Holm <tim.holm@nitric.io>
2023-05-17 14:03:16 +1000

    Add configurable alert title

--- cmd/root.go
+++ cmd/root.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/gen2brain/beeep"
 	"github.com/spf13/cobra"
 )
 
+const defaultTitle = "Poptart"
+
 var rootCmd = &cobra.Command{
 	Use:   "poptart",
 	Short: "Poptart is a simple CLI for generating notifications",
 	Long:  ``,
-	// Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
-		err := beeep.Alert("PopTart", args[0], "./cmd/poptart.png")
+		alertTitle, _ := cmd.Flags().GetString("title")
+		err := beeep.Alert(alertTitle, strings.Join(args, " "), "./cmd/poptart.png")
 		cobra.CheckErr(err)
 	},
 }
@@ -29,7 +32,7 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-	rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "Author name for copyright attribution")
+	rootCmd.PersistentFlags().StringP("title", "t", defaultTitle, "The title to set on the notification")
 }
 
 func initConfig() {
